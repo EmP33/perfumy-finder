616ad84d04dd22d0058405478e5132551d4d3e33
KacperPiotrowski <122211801+KacperPiotrowski@users.noreply.github.com>
2023-01-08 18:04:08 +0100

    add files

--- script.js
+++ script.js
@@ -1,145 +1,181 @@
+// links
+/**
+ * QR: https://davidshimjs.github.io/qrcodejs/
+ * 
+ */
+
 //enums 
-const states = {
+const statesEnum = {
     lang: "lang",
-    question: "question",
+    gender: "gender",
+    question: "questions",
     summary: "summary",
     perfumes: "perfumes"
 }
 
-const gender = {
+const genderEnum = {
     male: 3,
     female: 2
 }
 
 //variables
-let currentState = states.lang;
-let currentGender = gender.male;
+let currentState = statesEnum.lang;
 
 let currentQuestionNumber = -1;
 let countOfQuestions = 6;
 let currentLanguage = '';
 
-let answers = [];
+let questions = [];
+const answers = [];
 
-
-function onClickReturnButton() {
-    nextPage();
-    console.log(currentQuestionNumber);
+//functions
+function initial() {
+    setButtons();
+    showPage();
 }
 
-async function initial() {
-    //document.querySelector('button.return-btn').onclick = onClickReturnButton;
-    setContent();
-    countOfQuestions = getCountOfQuestions();
-    const questions = await downloadQuestions();
-    const selectedQuestions = randomizeQuestions(questions);
+function setButtons() {
+    Array.from(document.getElementsByClassName('language')).forEach((button) => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault();
+            let tempLang = e.target.getAttribute('data-language');
+            setLanguage(tempLang);
+            currentState = statesEnum.gender;
+            showPage();
+        });
+    });
+
+    Array.from(document.getElementsByClassName('gender')).forEach((button) => {
+        button.addEventListener('click', async (e) => {
+            e.preventDefault();
+            const tempGender = e.target.getAttribute('data-gender');
+            currentState = statesEnum.question;
+
+            // get count of quesions
+            countOfQuestions =
+                (tempGender == genderEnum.male)
+                    ? 6
+                    : 10;
+
+            // random questions
+            let selectedQuestions = await downloadQuestions();
+            selectedQuestions = filterQuestions(selectedQuestions, tempGender);
+            for (let i = 0; i < countOfQuestions; i++) {
+                const number = Math.floor(Math.random() * selectedQuestions.length);
+                selectedQuestions.push(questions[number]);
+            }
+            questions = selectedQuestions;
+            currentQuestionNumber++;
+            generateQuestions();
+            showPage();
+        });
+    });
 }
 
-function setContent() {
-    if (currentState != states.lang && currentQuestionNumber >= 2) {
-        document.querySelector(".return-btn").style.visibility = "visible";
-    }
-    else {
-        document.querySelector(".return-btn").style.visibility = "hidden";
-    }
-}
-
-function getCountOfQuestions() {
-    if (currentGender == gender.male) {
-        return 6;
-    }
-    return 10;
-}
-
-function previousPage() {
-
-}
-
-function nextPage() {
-    if (currentState == states.lang) {
-        currentState = states.question;
-
-    }
-    else if (currentState == states.question) {
-        if (currentQuestionNumber != countOfQuestions) {
-            return;
+function filterQuestions(questions, gender) {
+    const tmp = [];
+    questions.forEach(element => {
+        if (element.Q_GENDER == genderEnum[gender]) {
+            tmp.push(element);
         }
-        currentState = states.summary
-    }
-    else if (currentState == states.summary) {
-        currentState = states.perfumes;
+    });
+    return tmp;
+}
+
+function generateQuestions() {
+    //prepare
+    const questionGenericElement = document.querySelector('.question');
+    questionGenericElement.innerHTML = '';
+    const tmpQuestion = questions[currentQuestionNumber];
+    
+    //generate
+    document.querySelector('#txt-title').textContent=tmpQuestion.Q_QUESTION;
+    for (let j = 0; j < tmpQuestion.ANSWERS.length; j++) {
+        const answer = tmpQuestion.ANSWERS[j];
+
+        const button = genrateAnswerButton(answer.ANS_ID, answer.ANS_IMG, answer.ANS_TEXT);
+        questionGenericElement.appendChild(button);
     }
 }
+
+function generateAnswers() {
+    console.log(answers);
+    const questionGenericElement = document.querySelector('.summary > .answers');
+    questionGenericElement.innerHTML = '';
+    for (let i = 0; i < countOfQuestions; i++) {
+        const button = document.createElement("button");
+
+        questionGenericElement.append(button);
+    }
+}
+
+function genrateAnswerButton(id, imgSrc, pTxt) {
+    const button = document.createElement("button");
+    button.classList.add('answer-option');
+    button.setAttribute("data-answer", id);
+    button.style.backgroundImage = `url(${imgSrc})`;
+
+    const p = document.createElement("p");
+    p.textContent = pTxt;
+    button.appendChild(p);
+
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        const tempAttr = e.target.getAttribute('data-answer');
+
+        answers.push(tempAttr);
+
+        if (answers.length == countOfQuestions) {
+            currentState = statesEnum.summary;
+            showPage();
+            generateAnswers();
+        } else {
+            currentQuestionNumber++;
+            generateQuestions();
+        }
+    });
+
+    return button;
+}
+
+function showPage() {
+    const contentChildren = document.querySelectorAll('div#content > div');
+    contentChildren.forEach(child => {
+        child.style.display = "none";
+    });
+
+    const currentStateDiv = document.querySelector(`div[data-state="${currentState}"`);
+    currentStateDiv.style.display = "block";
+}
+
 
 async function downloadQuestions() {
-    let selectedQuestions = [];
-
+    let questionTmp;
     await fetch('./questions.json')
         //get data from promise
         .then(data => data.json())
         //manipulate data
         .then(data => {
-            let selectedQuestions = [];
+            const selectedQuestions = [];
             data.forEach(dat => {
-                if (dat.Q_GENDER === currentGender) {
-                    selectedQuestions.push(dat);
-                }
+                selectedQuestions.push(dat);
             });
-            return selectedQuestions;
-        })
-        // remove pending
-        .then(result => {
-            selectedQuestions = result;
+
+            questionTmp = selectedQuestions;
         });
 
-    return selectedQuestions;
-}
-
-function downloadTextAndReplace(lang) {
-    // Fetch the JSON file
-    fetch(`${lang}.json`)
-        .then(response => response.json())
-        .then(data => {
-            // Loop through the JSON data
-            for (const [key, value] of JSON.parse(data)) {
-                // Find all span elements with the matching data-text attribute
-                const spans = document.querySelectorAll(`span[data-text=${key}]`); t
-                // Replace the text of each span element
-                spans.forEach(span => span.textContent = value);
-            }
-        })
-        .catch(error => console.error(error));
-}
-
-function randomizeQuestions(questions) {
-    let selectedQuestions = [];
-
-    for (let i = 0; i < countOfQuestions; i++) {
-        const number = Math.floor(Math.random() * 35);
-        selectedQuestions.push(questions[number]);
-    }
-
-    return selectedQuestions;
+    return questionTmp;
 }
 
 async function setLanguage(lang) {
     //download the lang JSON file
-    await fetch('lang.{lang}.json')
+    await fetch(`lang.${lang}.json`)
         .then(response => response.json())
         .then(data => {
             currentLanguage = data;
         })
         .catch(error => console.error(error));
 }
-
-Array.from(document.getElementsByClassName('language')).forEach((button) => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        let tempLang = e.target.getAttribute('data-language');
-        setLanguage(tempLang);
-        
-    });
-});
 
 //on page load
 initial();
